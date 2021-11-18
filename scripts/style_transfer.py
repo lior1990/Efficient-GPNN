c9cfe91fdbfb17981dd7fc63153a59c83f8d30cf
unknown <lior1990@gmail.com>
2021-11-18 09:49:53 +0200

    add argparser for style transfer

--- scripts/style_transfer.py
+++ scripts/style_transfer.py
@@ -1,28 +1,23 @@
+import argparse
+import glob
 import os
 from time import time
 import sys
+from typing import List, Tuple
+import torch
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 from GPNN import PNN, GPNN
 from utils.image import save_image
 
-contents_and_styles = [
-    ('images/style_transfer/bin2.jpg', 'images/style_transfer/brick.jpg'),
-    ('images/style_transfer/hillary1.jpg', 'images/style_transfer/thick_oil.jpg'),
-    ('images/style_transfer/duck_mosaic.jpg', 'images/style_transfer/S_char.jpg'),
-    ('images/style_transfer/S_char.jpg', 'images/style_transfer/duck_mosaic.jpg'),
-    ('images/style_transfer/kanyon2.jpg', 'images/style_transfer/tower.jpg'),
-    ('images/style_transfer/tower.jpg', 'images/style_transfer/kanyon2.jpg'),
-    ('images/style_transfer/trump.jpg', 'images/style_transfer/mondrian.jpg'),
-]
 
+def main(contents_and_styles: List[Tuple[str, str]], out_dir: str):
+    device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
-if __name__ == '__main__':
     PNN_moduel = PNN(patch_size=7, stride=1, alpha=0.005, reduce_memory_footprint=True)
     GPNN_module = GPNN(PNN_moduel, scale_factor=(1, 1), resize=256, num_steps=10, pyr_factor=0.75, coarse_dim=128,
-                       noise_sigma=0, device="cuda:0")
+                       noise_sigma=0, device=device)
 
-    out_dir = f"outputs/style_transfer"
     for (content_image_path, style_iamge_path) in contents_and_styles:
         content_fname, ext = os.path.splitext(os.path.basename(content_image_path))[:2]
         style_fname, _ = os.path.splitext(os.path.basename(style_iamge_path))[:2]
@@ -31,3 +26,24 @@ if __name__ == '__main__':
             output_image = GPNN_module.run(target_img_path=style_iamge_path, init_mode=content_image_path)
             print(f"took {time() - start} s")
             save_image(output_image, os.path.join(out_dir, f'{GPNN_module.resize}x{GPNN_module.pyr_factor}->{GPNN_module.coarse_dim}', f"{content_fname}-to-{style_fname}${i}{ext}"))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--style_imgs_directory', type=str, required=True)
+    parser.add_argument('--content_imgs_directory', type=str, required=True)
+    parser.add_argument('--out_dir', type=str, default="output/style_transfer")
+    args = parser.parse_args()
+
+    style_imgs = glob.glob(f"{args.style_imgs_directory}/*.jpg")
+    content_imgs = glob.glob(f"{args.content_imgs_directory}/*.jpg")
+
+    print(f"Style imgs: {len(style_imgs)}")
+    print(f"Content imgs: {len(content_imgs)}")
+
+    contents_and_styles = []
+    for style_img in style_imgs:
+        contents_and_styles.extend([(content_img, style_img) for content_img in content_imgs])
+
+    print(f"Total number of images: {len(contents_and_styles)}")
+    main(contents_and_styles, args.out_dir)
