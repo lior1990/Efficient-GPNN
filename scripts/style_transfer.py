d74b38e02f9784a9fecad2a53374a0354c0d5e5c
unknown <lior1990@gmail.com>
2021-11-20 10:21:21 +0200

    restore patch size for double run

--- scripts/style_transfer.py
+++ scripts/style_transfer.py
@@ -26,6 +26,7 @@ def main(contents_and_styles: List[Tuple[str, str]], out_dir: str, coarse_dim: i
         img_name = os.path.join(out_dir, f'{GPNN_module.resize}x{GPNN_module.pyr_factor}->{GPNN_module.coarse_dim}', f"{content_fname}-to-{style_fname}{ext}")
         save_image(output_image, img_name)
         if double_run:
+            PNN_moduel = PNN(patch_size=7, stride=1, alpha=0.005, reduce_memory_footprint=True)
             GPNN_module = GPNN(PNN_moduel, scale_factor=(1, 1), resize=256, num_steps=10, pyr_factor=0.75,
                                coarse_dim=32, noise_sigma=0, device=device)
             output_image = GPNN_module.run(target_img_path=style_image_path, init_mode=img_name)
