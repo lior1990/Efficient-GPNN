8a3a3b18fbfdc54b5d37e644e390035188c6af2d
unknown <lior1990@gmail.com>
2022-01-03 15:35:42 +0200

    add argparse

--- scripts/reshuffle_mixed.py
+++ scripts/reshuffle_mixed.py
@@ -1,3 +1,4 @@
+import argparse
 import os
 from time import time
 import sys
@@ -12,8 +13,15 @@ device = "cuda:0" if torch.cuda.is_available() else "cpu"
 
 
 if __name__ == '__main__':
-    train_dataset_dir = 'images/2imgs'
-    eval_dataset_dir = 'images/eval'
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--train_dataset_dir', type=str, required=True)
+    parser.add_argument('--eval_dataset_dir', type=str, required=True)
+    parser.add_argument('--coarse_dim', type=int, default=14)
+    parser.add_argument('--noise_sigma', type=float, default=0)
+    args = parser.parse_args()
+
+    train_dataset_dir = args.train_dataset_dir
+    eval_dataset_dir = args.eval_dataset_dir
     train_image_paths = [os.path.join(train_dataset_dir, x) for x in os.listdir(train_dataset_dir)]
     eval_image_paths = [os.path.join(eval_dataset_dir, x) for x in os.listdir(eval_dataset_dir)]
 
@@ -26,8 +34,8 @@ if __name__ == '__main__':
                        resize=0,
                        num_steps=10,
                        pyr_factor=0.75,
-                       coarse_dim=14,
-                       noise_sigma=0,  # todo: add noise?
+                       coarse_dim=args.coarse_dim,
+                       noise_sigma=args.noise_sigma,  # todo: add noise?
                        device=device)
 
     out_dir = f"outputs/reshuffle_mixed"
